858e31c2e47d7793fe5b409df5fd020c5d70ae36
ParfenenkovEdit <parfenenkovedit@gmail.com>
2021-05-14 12:16:36 +0300

    feat: add methods for data manipulation in user derictory

---
+++ src/resources/repositories/user.memory.repository.js
@@ -0,0 +1,30 @@
+const User = require('../models/user.model');
+
+const users = [];
+
+const getAll = async () => users;
+
+const getById = async (userId) => {
+  const candidate = await users.find(user => user.id === userId);
+  return candidate;
+};
+
+const createUser = async ({ name, login, password }) => {
+  const user = await new User({ name, login, password });
+  return user;
+}
+
+const updateUser = async (omitObject) => {
+  const userIndex = users.findIndex(user => user.id === omitObject.id);
+  const updatedUser = await Object.assign(users[userIndex], omitObject);
+  users[userIndex] = updatedUser;
+  return updatedUser;
+}
+
+const deleteUser = async (userId) => {
+  const deletedUser = await users.splice(users.findIndex(user => user.id === userId), 1);
+  return deletedUser;
+}
+
+
+module.exports = { getAll, getById, createUser, updateUser, deleteUser };
---
+++ src/resources/service/user.service.js
@@ -0,0 +1,13 @@
+const usersRepo = require('../repositories/user.memory.repository');
+
+const getAll = () => usersRepo.getAll();
+
+const getById = (id) => usersRepo.getById(id);
+
+const createUser = ({ name, login, password }) => usersRepo.createUser({ name, login, password });
+
+const updateUser = ({ id, name, login, password }) => usersRepo.updateUser({ id, name, login, password });
+
+const deleteUser = (id) => usersRepo.deleteUser(id);
+
+module.exports = { getAll, getById, createUser, updateUser, deleteUser };
